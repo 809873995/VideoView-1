46f304d66b78de2b130c45160cd9f0279988f2f6
godliness <1229727237@qq.com>
2016-07-01 16:13:23 +0800

    videoview第一次提交

---
+++ app/src/main/java/com/sjzvideoview/android/utils/MediaInfor.java
@@ -0,0 +1,11 @@
+package com.sjzvideoview.android.utils;
+
+/**
+ * Created by sujizhong on 16/6/30.
+ */
+public class MediaInfor {
+
+    public String mName = "";
+
+    public String mPath = "";
+}
